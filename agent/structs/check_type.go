1db3414a09f74170785ba7a27347df8d86ac4c54
Matt Keeler <mjkeeler7@gmail.com>
2019-12-04 14:03:14 -0500

    Explicitly ignore ServiceID and service_id fields when decoding CheckType from JSON

--- agent/structs/check_type.go
+++ agent/structs/check_type.go
@@ -76,6 +76,11 @@ func (t *CheckType) UnmarshalJSON(data []byte) (err error) {
 		DockerContainerIDSnake              string      `json:"docker_container_id"`
 		TLSSkipVerifySnake                  bool        `json:"tls_skip_verify"`
 
+		// These are going to be ignored but since we are disallowing unknown fields
+		// during parsing we have to be explicit about parsing but not using these.
+		ServiceID      string `json:"ServiceID"`
+		ServiceIDSnake string `json:"service_id"`
+
 		*Alias
 	}{
 		Alias: (*Alias)(t),
